3dccabfdb13819c85d0c2f3cb22d0e0854123e82
tajpatel58 <tajpatel58@gmail.com>
2023-08-08 23:02:34 +0100

    adding function to parse data_dict into df

--- Scripts/data_parsing.py
+++ Scripts/data_parsing.py
@@ -1,17 +1,33 @@
 # Import packages:
 import json
+import pandas as pd
 from prefect import task, flow
 from Scripts.modelParams import ModelParams as Const
 
 @task
-def load_data(path : str) -> dict:
+def load_json_data(path : str) -> dict:
     with open(path, 'r') as f:
         data = json.load(f)
     return data
 
+
+@task
+def parse_json_data_to_dataframe(data : dict) -> pd.DataFrame:
+    data_dfs = []
+    for label, reviews_list in data.items():
+        sub_df = pd.DataFrame({Const.REVIEW : reviews_list})
+        sub_df[Const.LABEL_RAW] = label
+        data_dfs.append(sub_df)
+    parsed_df = pd.concat(data_dfs, axis=0)
+    return parsed_df
+
+
 @flow
 def load_data_flow():
-    load_data(Const.data_path)
+    data_dict = load_json_data(Const.data_path)
+    reviews_df = parse_json_data_to_dataframe(data_dict)
+
 
 if __name__ == "__main__":
     load_data_flow()
+    
--- Scripts/modelParams.py
+++ Scripts/modelParams.py
@@ -4,3 +4,6 @@ class ModelParams(object):
     app_home_dir = "/Users/tajsmac/Documents/Sentiment-Analysis"
     data_relative_path = "data/reviews.json"
     data_path = os.path.join(app_home_dir, data_relative_path)
+
+    REVIEW = "Review"
+    LABEL_RAW = "Sentiment"
