4cfb0fef094645af8c9013c57106a8476a7e2184
tajpatel58 <tajpatel58@gmail.com>
2023-12-14 00:42:49 +0000

    adding pythonic file naming convention

--- Scripts/featureEngineering.py
+++
@@ -1,7 +0,0 @@
-from prefect import task, flow
-from sklearn.decomposition import PCA
-
-@flow
-def pca_reduce_flow(X, n_comp : int):
-    reduced_mat = PCA(n_components=n_comp, random_state=42).fit_transform(X.values)
-    return reduced_mat
