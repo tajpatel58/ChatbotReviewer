ebaee81bad36e33d227d246d2fa865eb3d86d7c6
tajpatel58 <tajpatel58@gmail.com>
2023-12-27 18:29:26 +0000

    splitting fe into train/inference and adding funcs for pickling

--- src/modelling/feature_engineering.py
+++ src/modelling/feature_engineering.py
@@ -1,7 +1,9 @@
 import spacy
+import pickle
 import pandas as pd
 from prefect import task, flow
 from sklearn.decomposition import PCA
+from sklearn.preprocessing import LabelEncoder
 from src.modelling.model_params import ModelParams as Const
 
 
@@ -33,34 +35,96 @@ def create_word_embeddings_mat_task(
 
 
 @task
-def pca_reduce_task(X: pd.DataFrame, pca: PCA, inference=True):
-    if inference:
-        reduced_mat = pca.transform(X.values)
-    else:
+def pca_reduce_task(X: pd.DataFrame, pca: PCA, training: bool = True) -> None:
+    if training:
         reduced_mat = pca.fit_transform(X.values)
+    else:
+        reduced_mat = pca.transform(X.values)
     return reduced_mat
 
 
 @task
-def load_fitted_pca_task(path_to_pickle: str):
-    pass
+def load_pickled_preprocessing_objects_task(path_to_pickle: str) -> None:
+    try:
+        with open(path_to_pickle, "rb") as f:
+            pickle.load(f)
+    except FileNotFoundError:
+        print(f"Pickle at path: {path_to_pickle} not found!")
+    return None
+
+
+@task
+def pickle_fitted_preprocessing_objects_task(
+    path_to_pickle: str, preprocessing_objects: dict
+) -> None:
+    path_to_pickle.parent.mkdir(parents=True, exist_ok=True)
+    with open(path_to_pickle, "wb") as f:
+        pickle.dump(preprocessing_objects, f)
+    return None
+
+
+@task
+def transforming_labels_task(labels: pd.Series, label_encoder: LabelEncoder):
+    # group neutral reviews as negative:
+    replace_dict = {"neutral/constructive": "negative"}
+    replaced_labels = labels.map(replace_dict)
+    # transform labels:
+    labels = label_encoder.fit_transform(replaced_labels)
+    return labels
 
 
 @flow(validate_parameters=False, log_prints=True)
-def feature_engineering_flow(
-    cleaned_reviews_df: pd.DataFrame, inference: bool = True, n_components: int = None
+def feature_engineering_training_flow(
+    cleaned_reviews_df: pd.DataFrame,
+    path_to_pickle: str,
+    n_components: int,
 ) -> pd.DataFrame:
-    if inference:
-        path_to_pickle = " "
-        pca = load_fitted_pca_task(path_to_pickle=path_to_pickle)
-    else:
-        pca = PCA(n_components=n_components, random_state=42)
+    # initialise preprocessing objects:
+    pca = PCA(n_components=n_components, random_state=42)
+    label_encoder = LabelEncoder()
 
-    labels = cleaned_reviews_df[Const.BINARY_LABEL].values
+    # create word embeddings
     word_embeddings = create_word_embeddings_mat_task(
         cleaned_reviews_df, Const.REVIEW_NO_STOP_WORDS
     )
-    feature_matrix = pca_reduce_task(word_embeddings, pca)
+
+    # feature engineering/fitting preprocessing objects:
+    pca_reduced_matrix = pca_reduce_task(word_embeddings, pca, training=True)
+    labels = transforming_labels_task(
+        cleaned_reviews_df[Const.LABEL_RAW], label_encoder=label_encoder
+    )
+
+    # save preprocessing components
+    preprocessing_dict = {"pca": pca, "label_encoder": label_encoder}
+
+    pickle_fitted_preprocessing_objects_task(
+        path_to_pickle=path_to_pickle, preprocessing_objects=preprocessing_dict
+    )
+
     print("Top 5 rows and last 5 columns of feature matrix:")
-    print(feature_matrix[:5, -6:])
-    return feature_matrix, labels
+    print(pca_reduced_matrix[:5, -6:])
+    return pca_reduced_matrix, labels
+
+
+@flow(validate_parameters=False, log_prints=True)
+def feature_engineering_inference_flow(
+    cleaned_reviews_df: pd.DataFrame, path_to_pickle: str
+):
+    # load pickle file with preprocessing objects:
+    preprocessing_dict = load_pickled_preprocessing_objects_task(
+        path_to_pickle=path_to_pickle
+    )
+
+    # set preprocessng items:
+    pca = preprocessing_dict["pca"]
+
+    # create word embeddings
+    word_embeddings = create_word_embeddings_mat_task(
+        cleaned_reviews_df, Const.REVIEW_NO_STOP_WORDS
+    )
+
+    # feature engineering:
+    pca_reduced_matrix = pca_reduce_task(word_embeddings, pca, training=False)
+    print("Top 5 rows and last 5 columns of feature matrix:")
+    print(pca_reduced_matrix[:5, -6:])
+    return pca_reduced_matrix
