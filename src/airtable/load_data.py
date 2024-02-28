0c3411997c6fbe5728d9a83dc6f2d7d96be95cb1
Taj Patel <tajpatel58@gmail.com>
2024-02-28 22:33:36 +0000

    loading airtable vars from config and setting up app

--- src/airtable/load_data.py
+++ src/airtable/load_data.py
@@ -1,6 +1,11 @@
+import json
 import pandas as pd
 from pathlib import Path
 from pyairtable import Api
+from fastapi import FastAPI
+from src.airtable.api_schemas import Review
+
+app = FastAPI()
 
 
 def create_airtable_api(airtable_token_path: Path) -> Api:
@@ -8,6 +13,12 @@ def create_airtable_api(airtable_token_path: Path) -> Api:
         token = f.read()
     airtable_api = Api(token)
     return airtable_api
+
+
+def load_airtable_mapping_dict(path_to_json: Path) -> dict:
+    with open(path_to_json, "r") as f:
+        airtable_mapping_dict = json.load(f)
+    return airtable_mapping_dict
 
 
 def airtable_to_dataframe(
@@ -18,8 +29,19 @@ def airtable_to_dataframe(
     return data_df
 
 
-base_id = "appt3ILyWkGcmpceN"
-reviews_table_id = "tblNnw8GEfd5mcdY3"
-api = create_airtable_api("./token.txt")
-reviews_df = airtable_to_dataframe(api, reviews_table_id, base_id)
-print(reviews_df.head())
+@app.post("/add_reviews/")
+def add_review_to_airtable(review: Review):
+    # load airtable mapping dict:
+    path_to_airtable_keys = "./airtable_key_mapping.json"
+    airtable_ids_dict = load_airtable_mapping_dict(path_to_airtable_keys)
+
+    # connect to airtable API:
+    api = create_airtable_api("./token.txt")
+
+    # create airtable table connections:
+    base_id = airtable_ids_dict["base_id"]
+    reviews_table_id = airtable_ids_dict["classified_reviews_df"]
+    to_classify_reviews_id = airtable_ids_dict["reviews_to_classify_df"]
+    to_classify_reviews_df = api.table(base_id, to_classify_reviews_id)
+    reviews_table_df = api.table(base_id, reviews_table_id)
+    return review, to_classify_reviews_df, reviews_table_df
